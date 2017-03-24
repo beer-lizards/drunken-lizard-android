67e0a3a95c080bf20360b97b4ca9271763cf8f01
Cameron Rollheiser <icirellik@gmail.com>
2017-03-24 00:08:57 -0400

    Base Project
    Creating the base android project.

---
+++ app/src/main/java/com/drinkinglizards/DrunkenLizards.java
@@ -0,0 +1,13 @@
+package com.drinkinglizards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DrunkenLizards extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drunken_lizards);
+    }
+}
